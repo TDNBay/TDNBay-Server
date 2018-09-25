927dfd903570d30fdbec73d6fea0b5867d487d08
Lucas Amorim <loa@ic.ufal.br>
2018-09-25 16:10:26 -0300

    recebimento do json

--- serv.py
+++ serv.py
@@ -1,4 +1,6 @@
+import json
 import socket
+import struct
 
 port = 50000
 s = socket.socket()
@@ -9,9 +11,19 @@ s.listen(5)
 print('Listening....')
 
 
+
 while True:
     conn, addr = s.accept()
     print('Got connection from', addr)
+    data = []
+    head = s.recv(1)[0]
+    if not head: #JSON
+        content_len = s.recv(s.recv(1)[0])
+        lenc = struct.unpack("I", bytearray(content_len))[0]
+        json_bytes = s.recv(lenc)
+        obj = json.loads(json_bytes.decode("utf-8"))
+        print(obj)
+
     filename='cyber.mp4'
     f = open(filename,'rb')
     l = f.read(1048576)
